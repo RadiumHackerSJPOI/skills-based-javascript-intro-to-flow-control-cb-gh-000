77aee6ba5fa53d3f2b2a3aed9e44fd3d64ced44e
Saurabh Jain <radiumhacker@gmail.com>
2017-11-10 21:06:02 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -34,8 +34,9 @@ function ageChecker(age)
   }
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager(age) 
+{
+return if(13<age<19) ? console.log("You are a teenager") : console.log("You are not a teenager")
 }
 
 function switchAge(age) {
